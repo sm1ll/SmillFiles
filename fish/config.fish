b9002834e422707a366afd9c0369c301e2197f0b
sm1ll <140668708+sm1ll@users.noreply.github.com>
2026-09-04 20:14:35 -0400

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+fastfetch
+
+function starship_transient_prompt_func
+  starship module character
+end
+function starship_transient_rprompt_func
+  starship module custom.transient_time
+end
+starship init fish | source
